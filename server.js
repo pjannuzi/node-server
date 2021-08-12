69b03aea050a03bc84bf31f164d4650a0f5d41c9
Ubuntu <ubuntu@ip-172-31-1-191.us-east-2.compute.internal>
2021-08-12 09:17:41 +0000

    primeira comitada

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require ('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+	console.log("got a request");
+	res.send('Hello World');
+})
+
+app.listen(port, () => {
+	console.log('Example app listening at port 3000')
+})
