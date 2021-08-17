f439879f81e4115b6aba6fa4c1ca03a681ed800a
pjannuzi <pjannuzi@gmail.com>
2021-08-17 20:25:01 -0300

    novo layout hero banner

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,11 +1,13 @@
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
     document.getElementById("start").className = "bgg-white";
     document.getElementById("start1").className = "text2";
+    document.getElementById("nvb").className = "navbar-d";
   } else {
-    document.getElementById("start").className = "bgg-common";
+    document.getElementById("start").className = "";
     document.getElementById("start1").className = "text1";
+    document.getElementById("nvb").className = "navbarp";
   }
 }
