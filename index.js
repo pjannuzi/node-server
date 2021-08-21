ecf967462a586aaa9c30c07fb09b336f8c848dbb
pjannuzi <pjannuzi@gmail.com>
2021-08-20 22:12:56 -0300

    pagina 404 e att layout

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname + "/views/index.html")
 })
 
+app.use(function(req, res, next) {
+    res.status(404).sendFile(__dirname + "/views/404.html")
+});
+
 app.listen(port, () => {
 	console.log('App listening at port: 3000')
 })
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,12 +1,23 @@
+
+
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu() {
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
-  if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("start").className = "bgg-white";
     document.getElementById("start1").className = "text2";
     document.getElementById("nvb").className = "navbar-d";
   } else {
-    document.getElementById("start").className = "";
+    document.getElementById("start").className = "bgg-common";
     document.getElementById("start1").className = "text1";
     document.getElementById("nvb").className = "navbarp";
   }
