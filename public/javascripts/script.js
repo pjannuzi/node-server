e45571113b869fc462745d0aa5b1f6e073423a7e
pjannuzi <pjannuzi@gmail.com>
2021-08-22 01:59:17 -0300

    novo logo e slider

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -13,9 +13,6 @@ window.onscroll = function() {myFunction()};
 
 function myFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("start").className = "bgg-white";
-    document.getElementById("start1").className = "text2";
-    document.getElementById("nvb").className = "navbar-d";
     document.getElementById("btn-mobile").className = "btn-mobile";
     var x = document.getElementsByClassName("x");
     x[0].className = "x nvblack";
@@ -23,9 +20,6 @@ function myFunction() {
     x[2].className = "x nvblack";
     x[3].className = "x nvblack";
   } else {
-    document.getElementById("start").className = "bgg-common";
-    document.getElementById("start1").className = "text1";
-    document.getElementById("nvb").className = "navbarp";
     document.getElementById("btn-mobile").className = "btnnj";
     var x = document.getElementsByClassName("x");
     x[0].className = "x";
