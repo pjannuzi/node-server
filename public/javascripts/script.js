469eef52a335e00cae4ee4aeab1b8810fa51598f
pjannuzi <pjannuzi@gmail.com>
2021-08-22 04:22:23 -0300

    responsividade e ajustes layout

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -14,17 +14,7 @@ window.onscroll = function() {myFunction()};
 function myFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("btn-mobile").className = "btn-mobile";
-    var x = document.getElementsByClassName("x");
-    x[0].className = "x nvblack";
-    x[1].className = "x nvblack";
-    x[2].className = "x nvblack";
-    x[3].className = "x nvblack";
   } else {
     document.getElementById("btn-mobile").className = "btnnj";
-    var x = document.getElementsByClassName("x");
-    x[0].className = "x";
-    x[1].className = "x";
-    x[2].className = "x";
-    x[3].className = "x";
   }
 }
